d977ecefd0bc77d34ec09bc469c82c2dde3d70b8
Evan You <yyx990803@gmail.com>
2017-01-31 19:19:00 -0500

    tests

---
+++ test/test.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const HTMLStream = require('../index')
+const template = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8')
+const source = fs.readFileSync(path.join(__dirname, 'fixture.html'), 'utf-8')
+const sourceStream = fs.createReadStream(path.join(__dirname, 'fixture.html'), 'utf-8')
+
+test('should work', done => {
+  const context = {
+    head: '<title>hello</title>',
+    styles: '<style>h1 { color: red }</style>',
+    state: {
+      haxorXSS: '</script>'
+    }
+  }
+
+  const htmlStream = new HTMLStream({
+    template,
+    context
+  })
+
+  let output = ''
+  sourceStream
+    .pipe(htmlStream)
+    .on('data', chunk => {
+      output += chunk.toString()
+    })
+    .on('end', () => {
+      expect(output.indexOf(context.head)).toBeGreaterThan(-1)
+      expect(output.indexOf(context.head)).toBeGreaterThan(-1)
+      expect(output.indexOf(source)).toBeGreaterThan(-1)
+      expect(output.indexOf('{"haxorXSS":"\\u003C\\u002Fscript\\u003E"}')).toBeGreaterThan(-1)
+      done()
+    })
+})
